75d547c288978368bba474bf1a6da1ac8a28ef77
Markus Andersson <Markus.Andersson@beijerelectronics.com>
2023-02-27 16:41:19 +0100

    First commit

---
+++ DeviceApplication2/FileThread.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace DeviceApplication2
+{
+    public class FileThread
+    {
+        Form1 f;
+        int mode;
+        public FileThread(Form1 f)
+        {
+            this.f = f;
+        }
+
+        public int Mode
+        {
+            get { return this.mode; }
+            set { mode = value; }
+        }
+
+        public void FileProc()
+        {
+            for (int i = 0; i < 10; i++)
+            {                
+                String pathname = "Storage Card/test" + i;
+                switch (mode)
+                {
+                    case 0: //Create 
+                        createFile(pathname);
+                        break;
+                    case 1: //Write
+                        writeFile(pathname);
+                        break;
+                    case 2: //Delete
+                        deleteFile(pathname);
+                        break;
+                    default: //Nothing
+                        break;
+                }
+                //f.updateAsynch();
+                //createFile(pathname);
+                //deleteFile(pathname);   
+                Thread.Sleep(1000);
+
+            }
+        }
+
+        public void createFile(string name)
+        {
+            try
+            {
+                FileStream fs = File.Create(name);
+                //File.OpenWrite(pathname);
+                //File.WriteAllText(pathname, "hello world");
+                fs.Close();
+                System.Diagnostics.Trace.WriteLine("Created file " + name);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception thrown when creating file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button1);
+            }
+            
+
+
+        }
+
+        public void writeFile(string name)
+        {
+            if (File.Exists(name)) { 
+                using (StreamWriter writer = File.CreateText(name))
+                {
+                    writer.WriteLine("hello world");
+                }
+                System.Diagnostics.Trace.WriteLine("Wrote file " + name);
+            }
+            else
+            {
+                MessageBox.Show("File " + name + " does not exist.", "No file when writing", MessageBoxButtons.OK, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button1);
+            }
+        }
+        public void deleteFile(string name)
+        {
+            if (File.Exists(name))
+            {
+                if (File.Exists(name))
+                {
+                    File.Delete(name);
+                }
+                System.Diagnostics.Trace.WriteLine("Deleted file " + name);
+            }
+            else
+            {
+                MessageBox.Show("File " + name + " does not exist.", "No file when deleting", MessageBoxButtons.OK, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button1);
+            }
+           
+        }
+
+    }
+}
---
+++ DeviceApplication2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace DeviceApplication2
+{
+    public partial class Form1 : Form
+    {
+        int val;
+        TimeHandler tm;
+        bool ticktimer = false;
+        List<Thread> runningThreads;
+        private delegate void UpdateGUI();
+        public Form1()
+        {
+            InitializeComponent();
+            val = 0;
+            tm = new TimeHandler(this);
+            runningThreads = new List<Thread>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileThread ft = new FileThread(this);
+            ft.Mode = 0;
+            Thread t = new Thread(new ThreadStart(ft.FileProc));
+            runningThreads.Add(t);
+            t.Start();
+            System.Diagnostics.Trace.WriteLine("Started create!");
+            ticktimer = true;
+        }
+
+        public void updateAsynch()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new UpdateGUI(update));
+            }
+        }
+
+        private void update()
+        {
+            System.Diagnostics.Trace.WriteLine("Update! Val: " + val);
+            //Console.WriteLine("UPDATE");
+            val += 1;
+            label1.Text = Convert.ToString(val);
+            label1.Refresh();
+        }
+
+        public void updateTime(string s, string m, string h)
+        {
+            this.hourLabel.Text = h;
+            this.minLabel.Text = m;
+            this.sLabel.Text = s;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Thread t in runningThreads)
+            {
+                t.Join();
+            }
+            Application.Exit();
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FileThread ft = new FileThread(this);
+            ft.Mode = 1;
+            Thread t = new Thread(new ThreadStart(ft.FileProc));
+            runningThreads.Add(t);
+            t.Start();
+            System.Diagnostics.Trace.WriteLine("Started write!");
+            ticktimer = true;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileThread ft = new FileThread(this);
+            ft.Mode = 2;
+            Thread t = new Thread(new ThreadStart(ft.FileProc));
+            runningThreads.Add(t);
+            t.Start();
+            System.Diagnostics.Trace.WriteLine("Started delete!");
+            ticktimer = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (ticktimer)
+            {
+                tm.incSec();
+                //Console.WriteLine("TICK");
+                updateAsynch();
+            }
+        }
+    }
+}
