449067f0ae7e6a43a2a16700e61a3c5902686eea
mark9797 <78471307+mark9797@users.noreply.github.com>
2023-02-27 16:44:31 +0100

    First commit

---
+++ DeviceApplication2/DeviceApplication2/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace DeviceApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sLabel = new System.Windows.Forms.Label();
+            this.minLabel = new System.Windows.Forms.Label();
+            this.hourLabel = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.timer1 = new System.Windows.Forms.Timer();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular);
+            this.label1.Location = new System.Drawing.Point(406, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 48);
+            this.label1.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(651, 319);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 65);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Quit";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(483, 319);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 65);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(307, 319);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 65);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Write";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(798, 455);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.sLabel);
+            this.tabPage1.Controls.Add(this.minLabel);
+            this.tabPage1.Controls.Add(this.hourLabel);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(790, 426);
+            this.tabPage1.Text = "Main";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.label6.Location = new System.Drawing.Point(442, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 30);
+            this.label6.Text = "s";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.label5.Location = new System.Drawing.Point(387, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 30);
+            this.label5.Text = "m";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.label4.Location = new System.Drawing.Point(337, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 30);
+            this.label4.Text = "h";
+            // 
+            // sLabel
+            // 
+            this.sLabel.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.sLabel.Location = new System.Drawing.Point(411, 40);
+            this.sLabel.Name = "sLabel";
+            this.sLabel.Size = new System.Drawing.Size(34, 30);
+            this.sLabel.Text = "00";
+            // 
+            // minLabel
+            // 
+            this.minLabel.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.minLabel.Location = new System.Drawing.Point(358, 40);
+            this.minLabel.Name = "minLabel";
+            this.minLabel.Size = new System.Drawing.Size(34, 30);
+            this.minLabel.Text = "00";
+            // 
+            // hourLabel
+            // 
+            this.hourLabel.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.hourLabel.Location = new System.Drawing.Point(305, 40);
+            this.hourLabel.Name = "hourLabel";
+            this.hourLabel.Size = new System.Drawing.Size(34, 30);
+            this.hourLabel.Text = "00";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(790, 426);
+            this.tabPage2.Text = "Progress";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(798, 455);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label sLabel;
+        private System.Windows.Forms.Label minLabel;
+        private System.Windows.Forms.Label hourLabel;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
